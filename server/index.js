017e3f29bf086d47c69f6d91f42ce97dcf7bc470
Zechworld <ariel.herrera.s@gmail.com>
2022-03-29 14:19:18 -0300

    Server basic setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
+// Libraries
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+// Relative imports
+import postsRoutes from "./routes/posts.js"
+
+const PORT = process.env.PORT || 5000;
+const MONGO_CONNECTION_STRING =
+    "mongodb+srv://admin:aVidGNpiDQMXAR62@memoriesdb.46kco.mongodb.net/MemoriesDB?retryWrites=true&w=majority";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postsRoutes)
+
+await mongoose
+    .connect(MONGO_CONNECTION_STRING)
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));

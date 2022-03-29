190b696c442ed2c631097c6ee56d414e72d9511b
Zechworld <ariel.herrera.s@gmail.com>
2022-03-29 14:21:08 -0300

    Linter changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 // Relative imports
-import postsRoutes from "./routes/posts.js"
+import postsRoutes from "./routes/posts.js";
 
 const PORT = process.env.PORT || 5000;
 const MONGO_CONNECTION_STRING =
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postsRoutes)
+app.use("/posts", postsRoutes);
 
 await mongoose
     .connect(MONGO_CONNECTION_STRING)
